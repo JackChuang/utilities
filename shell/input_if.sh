750beec642610304488b74b565e9804e41496c77
JackChuang <horenc@vt.edu>
2017-05-23 17:58:57 -0400

    add one more shell script

---
+++ shell/input_if.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#
+# input.sh
+# Copyright (C) 2017 jackchuang <jackchuang@Jack-desktop>
+#
+# Distributed under terms of the MIT license.
+#
+
+echo "Input counts: $#"
+echo "First arg: $1"
+echo "Second arg: $2"
+if [ "$1" = "123" ];
+then
+    echo "123"
+elif [ "$1" = "456" ]; then
+    echo "456"
+else
+    echo "not 123/456"
+fi
