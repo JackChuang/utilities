48e37b55de2ec13d8c3b0f7cc9817be1bfdb2785
Ho-Ren(Jack) Chuang <horenc@vt.edu>
2017-09-23 03:49:55 -0400

    updated nsdi utilities

--- nsdi_uti/nsdi.py
+++ nsdi_uti/nsdi.py
@@ -20,53 +20,60 @@ timeDiff_3 = []
 filecnt=0
 
 ff = None # currFileName
-for TEST in [btb, epb, ftb, bk, numa_bfs, numa_page]: 
-    for i in [2, 3, 4]
-        for j in [sent, recv]
-            ff = str(TEST)+str(i)+"_"+str(j)
-            print(ff) #dbg
-            for curr in [1]:
+#for TEST in [btb, epb, ftb, bk, numa_bfs, numa_page]:
+#    for i in [2, 3, 4]
+for j in ["sent", "recv"]:
+    ff = "result_"+str(j)
+    print(ff) #dbg
+    for curr in [1]:
 #               print('try file: '+ str(ff))
-                try: 
-                    f = open(str(ff),'r')
-                    c_line = 0
-                    del var_1[:]
-                    for line in f:
-                        #if c_line == 0:
-                            #var_1[c_line] = float(line) #t[0]
-                            var_1.append(float(line)) #t[0]
-                            c_line += 1
-                        #else:
-                        #    print('Too many lines ')
-                        #end if
-                    #Do Math
-                    filecnt += 1
-                    #diff1 = var1 - 0	#
-                    
-                    #print('run '+ str(filecnt) +'\t\t1 '+str(diff1)+'\t2 '+str(diff2)+'\t3 '+str(diff3))
-                    #print('run '+ str(filecnt) +'\t\t1 '+str(diff1))
-                    
-                    #timeDiff_1.append(var1)
-
-                    #Do Avgerage avg for a file
-                    curAvg1=0
-                    for c in var_1:
-                            curAvg1 += c
-                    Aa1 = curAvg1/c_line
-                    print('Average by ' + str(c_line) + '\t' + str(Aa1))
-
-                    #Now time to save to file!
-                    filename ='jack_total.csv'
-                    with open(filename,'a') as csvv:
-                        writer = csv.writer(csvv)
-                        writer.writerow(str(Aa1))
+        try: 
+            f = open(str(ff),'r')
+            out = open(str(ff)+"_output" ,'2')
+            c_line = 0
+            del var_1[:]
+            for line in f:
+                #if c_line == 0:
+                #split(line)
+                line.split()
+                #var_1[c_line] = float(line) #t[0]
+                #var_1.append(float(line)) #t[0]
+                c_line += 1
+                #else:
+                #    print('Too many lines ')
+                #end if
                 
-                except: 
-                    pass
-                    #Add diff1 to end of array timeDiff_1
-            #end for ff (a file)
+                if (c_line == 4): 
+                    c_line = 0
+                    # calculate
 
-        #send/recv/ done
-    print('\n\n')
-    #next name
-#next benchmark
+                    # write to file
+                    
+            #Do Math
+            filecnt += 1
+            #diff1 = var1 - 0	#
+            
+            #print('run '+ str(filecnt) +'\t\t1 '+str(diff1)+'\t2 '+str(diff2)+'\t3 '+str(diff3))
+            #print('run '+ str(filecnt) +'\t\t1 '+str(diff1))
+            
+            #timeDiff_1.append(var1)
+
+            #Do Avgerage avg for a file
+            curAvg1=0
+            for c in var_1:
+                    curAvg1 += c
+            Aa1 = curAvg1/c_line
+            print('Average by ' + str(c_line) + '\t' + str(Aa1))
+
+            #Now time to save to file!
+            filename ='jack_total.csv'
+            with open(filename,'a') as csvv:
+                writer = csv.writer(csvv)
+                writer.writerow(str(Aa1))
+        
+        except: 
+            pass
+                    #Add diff1 to end of array timeDiff_1
+    #end for ff (a file)
+#send/recv/ done
+#print('\n\n')
