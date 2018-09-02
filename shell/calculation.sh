e41520ab031b918600824699713eff08c4601751
Ho-Ren(Jack) Chuang <horenc@vt.edu>
2018-09-02 16:56:17 -0400

    x

--- shell/calculation.sh
+++ shell/calculation.sh
@@ -31,6 +31,7 @@ echo "a2 - a1 = $SUM"
 echo $SUM
 
 
+#cat 10t_nl |grep  [0-9][0-9]. |awk '{print$5}'
 tasks="
 400000
 750000
@@ -43,7 +44,28 @@ tasks="
 15000000
 20000000
 "
+#cat 10t_nl |grep  [0-9][0-9]. |awk '{print$6}'
+times=(
+75241
+69762
+267122
+69863
+152014
+286301
+493150
+794520
+1212328
+1775342)
 
+a=0
 for i in $tasks; do
-    echo "10000000/$i"|bc -l
+    echo "10000000/$i*${times[$a]}/1000" |bc -l
+    #echo ${times[$a]}
+    let a=a+1
 done
+
+array=("123" "444" "666" "777")
+for i in "${arr[@]}"; do
+   echo "$i"
+done
+
