caeb509fb125c7946261dcbfb8cc92cdffa33492
Ho-Ren(Jack) Chuang <horenc@vt.edu>
2018-09-04 00:19:27 -0400

    x

---
+++ shell/lowecase.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#
+# input.sh
+# Copyright (C) 2017 jackchuang <jackchuang@Jack-desktop>
+#
+# Distributed under terms of the MIT license.
+#
+
+str=ABCDEFG
+echo ${str}
+echo ${str,,}
